aa8b770f8cbd38bb5553e23c5b147b5c89840d9b
Valerii Bondariev <valera.bondarev2005@gmail.com>
2024-10-26 14:19:04 +0200

    added bot delete members from chat, profile_kb

--- handlers/user_router.py
+++ handlers/user_router.py
@@ -1,12 +1,13 @@
+import asyncio
 from aiogram import Router, F
 from aiogram.filters import CommandStart, CommandObject, Command
 from aiogram.types import Message
 from create_bot import bot, channel_id
-from keyboards.kbs import main_kb
-from db_handler.db import get_user_data, insert_user
+from db_handler.db import get_user_data
+from keyboards.kbs import main_kb, profile_kb
 from utils.utils import get_refer_id, get_now_time
 from aiogram.utils.chat_action import ChatActionSender
-from aiogram.methods import CreateChatInviteLink
+from aiogram.methods import CreateChatInviteLink, BanChatMember, UnbanChatMember
 from datetime import timedelta
 
 user_router = Router()
@@ -18,7 +19,7 @@ async def cmd_start(message: Message, command: CommandObject):
         user_info = await get_user_data(user_id=message.from_user.id)
 
     if user_info:
-        response_text = f'{user_info.get("full_name")}, You already in channel.'
+        response_text = f'{user_info.get("full_name")}, hello there!'
     else:
         refer_id = get_refer_id(command.args)
         await insert_user(user_data={
@@ -45,11 +46,11 @@ async def get_profile(message: Message):
                 f'👥 Friends invited: <b>{user_info.get("count_refer")}</b>\n\n'
                 f'🚀 Link to invite your friends: '
                 f'<code>https://t.me/easy_refer_bot?start={message.from_user.id}</code>')
-    await message.answer(text, reply_markup=main_kb(message.from_user.id))
+    await message.answer(text, reply_markup=profile_kb(message.from_user.id))
 
 
 @user_router.message(Command('pay'))
-@user_router.message(F.text.contains('pay'))
+@user_router.message(F.text.contains('Pay subscription'))
 async def pay_command(message: Message):
     async with ChatActionSender.typing(bot=bot, chat_id=message.from_user.id):
 
@@ -58,6 +59,7 @@ async def pay_command(message: Message):
         # payment simulation
 
         user_id = message.from_user.id
+        await bot(UnbanChatMember(chat_id=channel_id, user_id=user_id))
         
         invite_link = await bot.create_chat_invite_link(
             chat_id=channel_id,
@@ -68,3 +70,20 @@ async def pay_command(message: Message):
         )
 
         await message.answer(f"Link to private channel: {invite_link.invite_link}\nYou have 5 minutes to activate link!")
+        asyncio.create_task(remove_user_after_delay(user_id, 20))
+
+    
+
+
+# test function
+async def remove_user_after_delay(user_id: int, delay: int):
+    await asyncio.sleep(delay)
+    await bot(BanChatMember(chat_id=channel_id, user_id=user_id, revoke_messages=True))
+    print(f"{user_id} has been banned")
+# delete
+
+
+# inv friends handler
+
+
+# check sub handler
--- keyboards/kbs.py
+++ keyboards/kbs.py
@@ -10,3 +10,17 @@ def main_kb(user_telegram_id: int):
         one_time_keyboard=True,
         input_field_placeholder="Menu"
     )
+
+
+def profile_kb(user_telegram_id: int):
+    kb_list = [
+        [KeyboardButton(text="Check sub")],
+        [KeyboardButton(text="Invite Friends")],
+        [KeyboardButton(text="Pay subscription")],
+    ]
+    return ReplyKeyboardMarkup(
+        keyboard=kb_list,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Profile Menu"
+    )
